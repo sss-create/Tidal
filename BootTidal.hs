3f43e20af54931fd8ba9a681742fbc3969467058
sss-create <72546851@posteo.jp>
2025-02-01 22:18:51 +0100

    my boottidal

--- BootTidal.hs
+++ BootTidal.hs
@@ -8,35 +8,38 @@ import Sound.Tidal.Boot
 
 default (Pattern String, Integer, Double)
 
--- Create a Tidal Stream with the default settings.
--- Use 'mkTidalWith' to customize these settings.
-tidalInst <- mkTidal
+-- ES8 48khz bei 128 Buffer = 0.066 latency
+:{
+let mTarget :: Target
+    mTarget = Target 
+      { oName      = "tidal"
+      , oAddress   = "127.0.0.1"
+      , oPort      = 9003
+      , oLatency   = 0.01
+      , oSchedule  = Live
+      , oWindow    = Nothing
+      , oHandshake = False
+      , oBusPort   = Nothing
+      }
 
--- This orphan instance makes the boot aliases work!
--- It has to go after you define 'tidalInst'.
+    tidalShape :: OSC
+    tidalShape = OSC "/tidal" $ Named {requiredArgs = ["s"]}
+
+    mOsc :: OscMap
+    mOsc = [(mTarget, [tidalShape])]
+
+    -- turing machine, outside value as steps, <~ shifts in time
+    turing steps len shift = outside steps (repeatCycles len) $ shift <~ rand
+
+    -- blofeld = "blofeld"
+
+    -- mininotation microtonality:
+    -- edo19 = 
+:}
+
+-- Create a Tidal Stream 
+tidalInst <- mkTidalWith defaultConfig mOsc
 instance Tidally where tidal = tidalInst
-
--- You can add your own aliases in this file. Here are some examples:
--- :{
--- let xfade i = transition tidal True (Sound.Tidal.Transition.xfadeIn 4) i
---     xfadeIn i t = transition tidal True (Sound.Tidal.Transition.xfadeIn t) i
---     histpan i t = transition tidal True (Sound.Tidal.Transition.histpan t) i
---     wait i t = transition tidal True (Sound.Tidal.Transition.wait t) i
---     waitT i f t = transition tidal True (Sound.Tidal.Transition.waitT f t) i
---     jump i = transition tidal True (Sound.Tidal.Transition.jump) i
---     jumpIn i t = transition tidal True (Sound.Tidal.Transition.jumpIn t) i
---     jumpIn' i t = transition tidal True (Sound.Tidal.Transition.jumpIn' t) i
---     jumpMod i t = transition tidal True (Sound.Tidal.Transition.jumpMod t) i
---     jumpMod' i t p = transition tidal True (Sound.Tidal.Transition.jumpMod' t p) i
---     mortal i lifespan release = transition tidal True (Sound.Tidal.Transition.mortal lifespan release) i
---     interpolate i = transition tidal True (Sound.Tidal.Transition.interpolate) i
---     interpolateIn i t = transition tidal True (Sound.Tidal.Transition.interpolateIn t) i
---     clutch i = transition tidal True (Sound.Tidal.Transition.clutch) i
---     clutchIn i t = transition tidal True (Sound.Tidal.Transition.clutchIn t) i
---     anticipate i = transition tidal True (Sound.Tidal.Transition.anticipate) i
---     anticipateIn i t = transition tidal True (Sound.Tidal.Transition.anticipateIn t) i
---     forId i t = transition tidal False (Sound.Tidal.Transition.mortalOverlay t) i
--- :}
 
 :set -fwarn-orphans
 :set prompt "tidal> "
